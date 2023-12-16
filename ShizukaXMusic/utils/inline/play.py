71b2219efdd0668b077b84069ca9a3a4f24edcc7
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2023-12-16 11:00:36 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -40,7 +40,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
            
-            ),
+            
         ],
         [
             InlineKeyboardButton(
