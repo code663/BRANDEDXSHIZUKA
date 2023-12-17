578f857563740012cfddf860f1c8f869537ba3ed
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2023-12-17 10:26:01 +0530

    Update ping.py

--- ShizukaXMusic/plugins/tools/ping.py
+++ ShizukaXMusic/plugins/tools/ping.py
@@ -5,20 +5,17 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS, MUSIC_BOT_NAME, PING_IMG_URL
 from strings import get_command
-
-from AnonX.core.call import Anon
-from AnonX.utils import bot_sys_stats
-from AnonX.utils.decorators.language import language
+from ShizukaXMusic import app
+from ShizukaXMusic.core.call import Shizuka
+from ShizukaXMusic.utils import bot_sys_stats
+from ShizukaXMusic.utils.decorators.language import language
 
 ### Commands
 PING_COMMAND = get_command("PING_COMMAND")
 
 
 @app.on_message(
-    filters.command(PING_COMMAND)
-    & filters.group
-    & ~filters.edited
-    & ~BANNED_USERS
+    filters.command(PING_COMMAND) & filters.group & ~filters.edited & ~BANNED_USERS
 )
 @language
 async def ping_com(client, message: Message, _):
@@ -27,11 +24,10 @@ async def ping_com(client, message: Message, _):
         caption=_["ping_1"],
     )
     start = datetime.now()
-    pytgping = await Anon.ping()
+    pytgping = await Shizuka.ping()
     UP, CPU, RAM, DISK = await bot_sys_stats()
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
-        _["ping_2"].format(
-            resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping
-        )
+        _["ping_2"].format(resp, MUSIC_BOT_NAME, UP, RAM, CPU, DISK, pytgping)
     )
+    
