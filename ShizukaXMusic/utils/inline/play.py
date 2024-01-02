0632b39b43957a9a4344d5262dcd3c8ec72a75c6
MRPAWANMUSIC <146386254+code663@users.noreply.github.com>
2024-01-03 00:22:11 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "📍—————————"
+        bar = "⚘—————————"
     elif 10 < umm < 20:
-        bar = "—📍————————"
+        bar = "—⚘————————"
     elif 20 <= umm < 30:
-        bar = "——📍———————"
+        bar = "——⚘———————"
     elif 30 <= umm < 40:
-        bar = "———📍——————"
+        bar = "———⚘——————"
     elif 40 <= umm < 50:
-        bar = "————📍—————"
+        bar = "————⚘—————"
     elif 50 <= umm < 60:
-        bar = "—————📍————"
+        bar = "—————⚘————"
     elif 60 <= umm < 70:
-        bar = "——————📍———"
+        bar = "——————⚘———"
     elif 70 <= umm < 80:
-        bar = "———————📍——"
+        bar = "———————⚘——"
     elif 80 <= umm < 95:
-        bar = "————————📍—"
+        bar = "————————⚘—"
     else:
-        bar = "—————————ᴇɴᴅ🥺"
+        bar = "—————————⚘"
     buttons = [
                 [
             InlineKeyboardButton(
@@ -62,21 +62,20 @@ def stream_markup_timer(_, chat_id, played, dur):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
                  [
             InlineKeyboardButton(
 
-                text="🍒‌⃰‌˶‌֟፝σɯиєя🧸",
+                text="◉ ᴏᴡɴᴇʀ ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="🍒‌⃰‌˶‌֟፝ѕυρρσят🧸",
+                text="◉ sᴜᴘᴘᴏʀᴛ ",
 
                 url=f"{SUPPORT_CHAT}",
 
@@ -92,21 +91,20 @@ def stream_markup(_, chat_id):
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
             InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
                  [
             InlineKeyboardButton(
 
-                text="🍒‌⃰‌˶‌֟፝σɯиєя🧸",
+                text="◉ ᴏᴡɴᴇʀ ",
 
                 url=f"t.me/{OWNER_USERNAME}",
 
             ),
             InlineKeyboardButton(
 
-                text="🍒‌⃰‌˶‌֟፝ѕυρρσят🧸",
+                text="◉ sᴜᴘᴘᴏʀᴛ",
 
                 url=f"{SUPPORT_CHAT}",
 
